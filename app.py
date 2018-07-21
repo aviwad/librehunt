fe42a07d9290589e8010481f3d0e12e941715514
Avi Wadhwa <aviwad@gmail.com>
2018-07-21 23:04:01 +0530

    add logo's, add distro codenames, start building algorithm, add branch

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
-#@app.route("/chooser", methods=["GET", "POST"])
-#def chooser():
+
+@app.route("/chooser") #, methods=["GET", "POST"])
+def chooser():
 #    if request.method == "POST":
 #        return render_template('recommendations.html', )
-#    return render_template('chooser.html')
-#@app.route("/recommendations")
-#def recommendations():
+    return render_template('chooser.html')
 
-#@app.route("/feedback")
-#def feedback():
-#    return render_template('feedback.html')
-#@app.route("/about")
-#def about():
-#    return render_template('about.html')
+@app.route("/recommendations")
+def recommendations():
+    return render_template('recommendations.html', )
+
+@app.route("/feedback")
+def feedback():
+    return render_template('feedback.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
