5374033d95b8eeeac4eb80576125dfb04e57151a
Avi Wadhwa <aviwad@gmail.com>
2018-09-30 09:45:38 +0530

    fix logic for algorithm.. again. My idea was right earlier but I was too sleepy to make it work properly lol

--- __init__.py
+++ __init__.py
@@ -54,20 +54,23 @@ def index():
             # loop through similarvariables
             for similarvariable in range(7):
                 # if similar variable is filter variable
-                if similarvariable == 2 or similarvariable == 4 or similarvariable == 5:
+                if similarvariable == 0 or similarvariable == 2 or similarvariable == 3:
                     # if current filter variable is not 0
-                    if similarvariable != 0:
+                    if distro[similarvariable+2] != 0:
                         # then continue and add a 1 to the array at the end coresponding to similarvariable if the distro and user have different values for the property
-                        if UserOptions[similarvariable-1] != distro[similarvariable]:
+                        if UserOptions[similarvariable+1] != distro[similarvariable+2]:
                             if len(distro[similarvariable+14]) == 1:
                                 distro[similarvariable+14][0]=1
                             else:
                                 distro[similarvariable+14].append(1)
-                elif UserOptions[similarvariable-1] != distro[similarvariable]:
+                elif UserOptions[similarvariable+1] != distro[similarvariable+2]:
                     if len(distro[similarvariable+14])==1:
                         distro[similarvariable+14][0]=1
                     else:
                         distro[similarvariable+14].append(1)
+                if similarvariable==7:
+                    print("distro customtweaks: "+str(distro[similarvariable+14]))
+                    print("user customtweaks: "+str(UserOptions[similarvariable-1]))
             # add a 0, so when totaling and sorting descending, it doesn't give error
             distro[21].append(0)
             for similarvariable in range(7):
